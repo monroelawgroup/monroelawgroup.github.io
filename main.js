dfdaa9937d20fc1c26a81b32c4291ae09d1d7ab2
Ezekiel Lopez <EzekielAdamLopez@gmail.com>
2022-01-13 23:31:55 -0800

    Added drop down menu

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 window.addEventListener("load", async function main() {
   const $ = (str, dom=document) => [...dom.querySelectorAll(str)];
   const wait = t => new Promise(r => setTimeout(r, t));
-  
+
+  const header = $("header")[0];
+  const rootStyle = document.documentElement.style;
+  function resize() {
+    const headerHeight = header.offsetHeight;
+    rootStyle.setProperty('--header-height', headerHeight+"px");
+  }
+  resize();
+  window.addEventListener("resize", resize);
+
+  const brokenElements = $(".broken");
+  brokenElements.forEach(el => {
+    el.addEventListener("click", e => {
+      e.preventDefault();
+      alert("This link is down for matinence");
+    });
+  });
 });
