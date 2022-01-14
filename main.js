f8f508b3a944c97ce8df83da05deb2d95bd39ad6
Ezekiel Lopez <EzekielAdamLopez@gmail.com>
2022-01-13 19:04:34 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", async function main() {
+  const $ = (str, dom=document) => [...dom.querySelectorAll(str)];
+  const wait = t => new Promise(r => setTimeout(r, t));
+  
+});
